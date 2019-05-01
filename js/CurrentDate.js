e6b8e5aebf6212e806592da53e140e7fec1941fb
Nicholas Donovan Hudson <nicholashudsonnk@gmail.com>
2019-05-01 09:02:02 -0600

    test js

--- js/CurrentDate.js
+++ js/CurrentDate.js
@@ -3,3 +3,4 @@ let today = new Date(); // new Date object
 // now concatenate formatted output
 let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFullYear();
 document.getElementById('currentdate').innerHTML = date;
+alert "test"
