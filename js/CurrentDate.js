2088dfe74d017fe60e0b504a24e0967437bef11b
ickynk <parkourkingsnk@yahoo.com>
2019-05-01 12:30:37 -0600

    js

--- js/CurrentDate.js
+++ js/CurrentDate.js
@@ -1,6 +1,4 @@
-// Get Current Date
-let today = new Date(); // new Date object
-// now concatenate formatted output
+let today = new Date(); 
 let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFullYear();
 document.getElementById('currentdate').innerHTML = date;
 alert("Test Message");
