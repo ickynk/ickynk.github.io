537bbbfc0b0803045bf7c30f76e470a929b177c2
ickynk <parkourkingsnk@yahoo.com>
2019-05-23 20:31:59 -0600

    fixes

--- Week 5/aftertax/js/aftertax.js
+++ Week 5/aftertax/js/aftertax.js
@@ -7,7 +7,9 @@ document.getElementById("currentdate").innerHTML = date;
 
 /*Design Table
 INPUT: Get hours worked and standard wage from number boxes. hr and w
-PROCESSING: Multiply hr by w and then multiply the total by 85%. hr * w * .85 = sum
+PROCESSING: Multiply hr by w and then multiply the total by 85%. hr * w * .85 = num ; 
+To ensure the output only uses two decimal points the problem is broken down to solve the equation then utilze the toFixed() function to limit decimal points
+the limited equation is then concatenated into a string for output.
 OUTPUT: Display result in document
 */
 // Designate HTML id's to be used within JS. Pulls information input into form and allows JS to assign numeric value to hr, w and output.
@@ -19,8 +21,13 @@ function math() {
   if (hr, w == "0") {
     let output = "Please enter Hours worked and Standard Wage"
   document.getElementById("output").innerHTML = output;
+  } else if (hr, w == "") {
+    let output = "Please enter Hours worked and Standard Wage"
+  document.getElementById("output").innerHTML = output;
   } else {
-  let output = "$ " + (hr * w) * .85
+  let num = (hr * w) * .85;
+  var n = num.toFixed(2);
+  let output = "$ " + n ; 
 //OUTPUT
   document.getElementById("output").innerHTML = output;
 }}
--- Week 5/cylinder/js/cylinder.js
+++ Week 5/cylinder/js/cylinder.js
@@ -13,8 +13,8 @@ OUTPUT: Updates the output value in HTML and displays on screen.
 
 // INPUT
 function math() {
-  let r = document.getElementById("r").value;
-  let h = document.getElementById("h").value;
+  let r = parseFloat(document.getElementById("r").value);
+  let h = parseFloat(document.getElementById("h").value);
   let output = document.getElementById("output");
 // PROCESSING
   let volume = Math.PI * Math.pow (r, 2) * h;
