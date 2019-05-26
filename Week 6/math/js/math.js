ecf424b8c0d19748bf7a892282f826d0dc7dffd8
ickynk <parkourkingsnk@yahoo.com>
2019-05-25 22:28:12 -0600

    design table

--- Week 6/math/js/math.js
+++ Week 6/math/js/math.js
@@ -5,18 +5,21 @@ let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFu
 document.getElementById('currentdate').innerHTML = date;
 
 /* Design Table
-INPUT: HTML generates a series of options for the user to select. JS will receive input from the user. 
-PROCESSING: Using a switch method the input will be matched with one of the options. This will generate a value for "shoe"
-OUTPUT: Concatenates a message along with the shoe value.
+INPUT 1: Once a user hits a button (reset) which starts the first process.  
+PROCESSING 1: javascript utilizes a randomized function to select a number between 1-100. This process is done for two variables (a and b). 
+OUTPUT 1: Displays a and b on the screen. 
+INPUT 2: Requests a user to input the answer for the sum of both a and b values input is (ans). The user then clicks a button to verify the answer. 
+PROCESSING 2: On click javascript begins an else if sequence. if ans is equal to a + b then it alters (output) to an affirmative statement, otherwise it alters (output) to a negative statement. 
+OUTPUT 2: Displays output underneath the problem. 
 */
-//INPUT
-//PROCESSING
+//INPUT 1 in HTML
+//PROCESSING 1
 function genQuestion(max, min) {
   min = Math.ceil(1);
   max = Math.floor(101);
   var a = Math.floor(Math.random() * (max - min)) + min;
   var b = Math.floor(Math.random() * (max - min)) + min;
-//OUTPUT
+//OUTPUT 1
     document.getElementById("a").value = a;
     document.getElementById("b").value = b;
     let output = "Good luck!"
@@ -25,15 +28,20 @@ function genQuestion(max, min) {
     document.getElementById("ans").value = ans;
   
 }
+//INPUT 2 in HTML
+//PROCESSING 2
 function checkAns() {
   let ans = parseInt(document.getElementById("ans").value);
   let a = parseInt(document.getElementById("a").value);
   let b = parseInt(document.getElementById("b").value);
   if (a + b == ans) {
     let output = "Correct! Good job."
+//OUTPUT 2
     document.getElementById("output").innerHTML = output;
+//PROCESSING 2
   } else {
     let output = "Sorry. That is incorrect."
+//OUTPUT 2
     document.getElementById("output").innerHTML = output;
   }
 }
